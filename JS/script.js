39b5449f5e88e8e73f5f0f59d482f36c19fc98d3
DanyMencu <dany69men@gmail.com>
2021-11-02 15:44:25 +0100

    Project created

---
+++ JS/script.js
@@ -0,0 +1,7 @@
+//Indicare il livello di difficoltà
+//Generare una griglia quadrata in relazione alla difficoltà
+//con difficoltà 1 => tra 1 e 100
+//con difficoltà 2 => tra 1 e 81
+//con difficoltà 3 => tra 1 e 49
+//Inserire in ogni quadrato il proprio numero corrispondente
+//Quando l'utente clicca su un quadrato diventa blu
