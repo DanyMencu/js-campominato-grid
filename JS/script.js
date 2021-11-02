a500c5a5bc7ec9dfc91fa97ae34d34684b7da5a1
DanyMencu <dany69men@gmail.com>
2021-11-02 17:32:51 +0100

    update

--- JS/script.js
+++ JS/script.js
@@ -26,14 +26,16 @@ setBtn.addEventListener('click',() => {
     
     switch (gridDimension) {
         case '1' :
+            cellsNumber = 100;
+            cellsPerSide = 10;
+            break;
+        case '2' :
+            cellsNumber = 81;
+            cellsPerSide = 9;
+            break;
+        case '3' :
             cellsNumber = 49;
             cellsPerSide = 7;
-            case '2' :
-                cellsNumber = 81;
-                cellsPerSide = 9;
-                case '3' :
-                    cellsNumber = 100;
-            cellsPerSide = 10;
         }
         console.log(cellsNumber);
         console.log(cellsPerSide);
@@ -44,4 +46,23 @@ setBtn.addEventListener('click',() => {
 
         //Add grid
         wrapGrid.append(grid);
+
+        //Gen grid square
+        for (let i = 1; i <= cellsNumber; i++) {
+            const square = createGridSquare (cellsPerSide);
+            square.innerHTML += `${i}`;
+
+            grid.append(square);
+        }
     });
+
+    //Functions
+
+    function createGridSquare (cells) {
+        const node = document.createElement('div');
+        node.classList.add('square');
+        node.style.width = `calc( 100% / ${cells} )`;
+        node.style.height = `calc( 100% / ${cells} )`;
+
+        return node;
+    }
